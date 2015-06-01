99f123717149d2220e97f89071e3b48f122b9534
Rémi Jarasson <remi.jarasson@ext.betc.com>
2015-06-01 18:49:56 +0200

    Url Helper for destroy_sms_session

--- lib/devise_sms_authenticatable/controllers/url_helpers.rb
+++ lib/devise_sms_authenticatable/controllers/url_helpers.rb
@@ -2,7 +2,7 @@ module DeviseSmsAuthenticatable
   module Controllers
     module UrlHelpers
       [:path, :url].each do |path_or_url|
-        [nil, :new_, :send_sms_, :resend_].each do |action|
+        [nil, :new_, :destroy_, :send_sms_, :resend_].each do |action|
           class_eval <<-URL_HELPERS, __FILE__, __LINE__ + 1
             def #{action}sms_session_#{path_or_url}(resource, *args)
               resource = case resource
